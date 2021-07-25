77661ce2bb8b261bdc001055d6842afaaf71de77
Zacharyjg <guldb016@umn.edu>
2021-07-25 12:10:31 -0500

    Added in front end click handling for when a user submits a comment

---
+++ public/js/comment.js
@@ -0,0 +1,30 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+    const content = document.querySelector('#comment-content').value.trim();
+
+    if (content) {
+        console.log(event.target);
+        if (event.target.hasAttribute('data-id')) {
+            const post_id = event.target.getAttribute('data-id');
+            const response = await fetch(`/api/post`, {
+                method: 'POST',
+                body: JSON.stringify({ content, post_id }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                document.location.replace(`/post/${post_id}`);
+            } else {
+                alert('Failed to create post');
+            }
+        }
+    }
+};
+
+if (document.querySelector("#submit-comment")) {
+    document
+        .querySelector('#submit-comment')
+        .addEventListener('click', newCommentHandler);
+}
