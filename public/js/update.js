efdc980c5b5c18bc42be0e962256d98790e6a385
Zacharyjg <guldb016@umn.edu>
2021-07-24 23:41:54 -0500

    Added update.js file to deal with updating a post

---
+++ public/js/update.js
@@ -0,0 +1,29 @@
+const updateFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+
+    if (title && content) {
+        if (event.target.hasAttribute('data-id')) {
+            const id = event.target.getAttribute('data-id');
+            const response = await fetch(`/api/posts/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({ title, content }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert('Failed to update post');
+            }
+        }
+    }
+};
+
+document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', updateFormHandler);
