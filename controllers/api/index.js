eced00634a0f8667e226dc7773c0f6ef38737f41
Zacharyjg <guldb016@umn.edu>
2021-07-25 12:13:25 -0500

    Changed routing slightly so that all of the routes point to the proper thing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const postRoutes = require("./postRoutes");
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use("/post", postRoutes);
 
 module.exports = router;
